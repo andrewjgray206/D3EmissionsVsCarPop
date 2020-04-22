638be91d2584485490b920b4c5f2132df052d476
Andrew Gray <37289566+andrewjgray206@users.noreply.github.com>
2020-04-22 23:06:42 +1000

    wireframe iteration 1
    svg created, absolute website basics

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 function init(){
+    d3.csv("projectdata.csv").then(function(data){
+        dataset = data;
+        console.log("readininfo",dataset);
+        createScatter(dataset);
+    });
+
 }
+
+function createScatter(dataset){
+    var w = 700;
+    var h = 200;
+    var barPadding=10;
+
+    if (dataset){
+        var svg = d3.select("#svgtoedit")
+                .append("svg")
+                .attr("width",w)
+                .attr("height",h);
+    }
+    else{
+        alert("Error loading data...");
+    }
+
+
+}
+window.onload=init;
