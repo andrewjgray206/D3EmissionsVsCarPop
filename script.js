3e41ae4cd01b021f7949d216b990378d14577866
Andrew Gray <37289566+andrewjgray206@users.noreply.github.com>
2020-06-08 12:03:43 +1000

    + part implementation of comparison

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//loads in the data, initialises the visualisation.
 function init(){
     d3.csv("projectdata2.csv").then(function(data){
 
@@ -8,21 +9,77 @@ function init(){
     });
 
 }
+/********************************
+ * The following function is used to
+ * improve the coherence and interactivity
+ * of the visualisation, giving users
+ * a chance to compare and see the
+ * changes calculated between
+ * two years of their choice.
+ *******************************/
+
+function compareInit(){ 
+    var year1Val = document.getElementById("year1").value;
+    var year2Val = document.getElementById("year2").value;
+    console.log("year1=",year1Val);
+    console.log("year2=",year2Val);
+
+    d3.csv("projectdata2.csv").then(function(data){
+        csvArray = data;
+
+        data.forEach(function(d){
+            if (year1Val == d.year){
+                compare1year = d.year;
+                compare1emissions = d.emissions;
+                compare1carpop = d.carpop;
+            }
+            if (year2Val == d.year){
+                compare2Year = d.year;
+                compare2emissions = d.emissions;
+                compare2carpop = d.carpop;
+            }
+            if ((compare1year) && (compare2Year)){
+                
+                var comparediv = d3.select("#svgtoedit")
+                                    .append("div")
+                                    .append("class","tooltip")
+                                    .style("opacity",1);
+            }
+
+        })
+
+    })
+
+}
+
+function compareData(csvArray,year1Val,year2Val){
+
+}
+
+/***************************************
+ * The following function creates the
+ * visualisation itself using D3v5.
+ * It reads in the csv dataset and
+ * plots onto an appropriate 
+ * scatter plot.
+ * Tooltips providing specific
+ * point information can be found and utilised.
+ **************************************/
 
 function createScatter(dataset){
 
-    var w = 900;
-    var h = 500;
+    var w = 900; //width of the svg
+    var h = 500; //height of the svg
     var padding=90;
 
     if (dataset){ //if the dataset exists (eg reads in correctly)
 
-        var xScale = d3.scaleLinear()
-                        .domain([320000,400000])
+        var xScale = d3.scaleLinear() 
+                        .domain([320000,400000]) //values which the dataset lies 
                         .range([padding,w-padding]);
 
         var yScale = d3.scaleLinear()
-                        .domain([12000000,18000000])
+                        .domain([12000000,18000000]) //values which the dataset lies.
                         .range([h-padding,padding]);
 
         var xAxis = d3.axisBottom()
@@ -31,18 +88,18 @@ function createScatter(dataset){
         var yAxis = d3.axisLeft()
                         .scale(yScale);
 
-        var svg = d3.select("#svgtoedit")
+        var svg = d3.select("#svgtoedit") //creates the svg
                 .append("svg")
                 .attr("width",w)
                 .attr("height",h)
                 .attr("fill","grey");
         
-        var tooltip = d3.select("#svgtoedit")
+        var tooltip = d3.select("#svgtoedit") //creates the tooltip.
                         .append("div")
                         .attr("class","tooltip")
                         .style("opacity",0);
         
-        svg.selectAll("circle")
+        svg.selectAll("circle") //plots the points on the graph.
             .data(dataset)
             .enter()
             .append("circle")
@@ -56,7 +113,7 @@ function createScatter(dataset){
             .attr("fill","red")
             .on("mouseover",function(d){
                 d3.select(this)
-                    .attr("fill","yellow");
+                    .attr("fill","yellow"); //mouseover goes yellow.
 
                 var xPos = parseFloat(d3.select(this).attr("cx"))
                 var yPos = parseFloat(d3.select(this).attr("cy"))
@@ -69,27 +126,32 @@ function createScatter(dataset){
                 
                 tooltip.html(html)
                         .style("left",(xPos-40)+"px")
-                        .style("top",(yPos+80)+"px")
+                        .style("top",(yPos+150)+"px")
                         .transition()
-                        .duration(200)
+                        .duration(500)
                         .style("opacity",0.9)
             })
             .on("mouseout",function(d,i){
                 d3.select(this)
                     .attr("fill","red");
-                tooltip.style("opacity",0);
+                tooltip.transition()
+                        .duration(500)
+                        .style("opacity",0);
             });
         
-        svg.append("g")
+        svg.append("g") //creates the xAxis line.
             .attr("transform", "translate(0+"+ (h - padding) +")")
             .call(xAxis);
-        svg.append("g")
+
+        svg.append("g") //creates the yAxis line.
             .attr("transform","translate(0"+(padding)+")")
             .call(yAxis);
-        svg.append("text")
+
+        svg.append("text") //creates the xAxis Label
             .attr("transform","translate(" + (300)+ " ," + (h-20)+ ")")
             .text("Carbon Emissions of Australia in KiloTonnes");
-        svg.append("text")
+
+        svg.append("text") //creates the yAxis Label.
             .attr("transform","rotate(-90)")
             .attr("y",20)
             .attr("x",-300)
